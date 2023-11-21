0dfa1a69e2af92059786e4b5b9739a5e91169864
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2023-11-22 00:25:59 +0700

    feat: add new post api

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
 from django.http.response import JsonResponse
+
 from posts.models import Post
 from posts.serializers import PostSerializer
 
@@ -12,9 +15,21 @@ def post_list(request):
     posts = Post.objects.all()
     post_serializer = PostSerializer(posts, many=True)
 
-    return JsonResponse(post_serializer.data, safe=False)
+    return JsonResponse(post_serializer.data, safe=False, status=status.HTTP_200_OK)
     
+  elif request.method == 'POST':
+    post_data = JSONParser().parse(request)
+    post_serializer = PostSerializer(data=post_data)
+    if (post_serializer.is_valid()):
+      post_serializer.save()
+      return JsonResponse(post_serializer.data, status=status.HTTP_201_CREATED)
+
+    return JsonResponse(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def post_detail(request, id):
-  return False
+  try:
+    post = Post.objects.get(id=id)
+  except:
+    return JsonResponse({'message': 'The post does not exist'}, status=status.HTTP_404_NOT_FOUND)
