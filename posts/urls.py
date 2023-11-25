d63789fd28a081a35b7855110a189e46720d5fd1
Phuoc Tai <87972973+phuoctai1644@users.noreply.github.com>
2023-11-25 09:06:02 +0700

    feat: handle GET post<id>/comment

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ from posts import views
 
 urlpatterns = [
     path('', views.post_list),
-    path('<int:id>', views.post_detail)
+    path('<int:id>', views.post_detail),
+    path('<int:id>/comments', views.post_comments)
 ]
--- posts/views.py
+++ posts/views.py
@@ -8,6 +8,9 @@ from django.http.request import HttpRequest
 from posts.models import Post
 from posts.serializers import PostSerializer
 
+from comments.models import Comment
+from comments.serializers import CommentSerializer
+
 @api_view(['GET', 'POST'])
 def post_list(request: HttpRequest):
   # Get all post
@@ -53,5 +56,15 @@ def post_detail(request: HttpRequest, id):
         status=status.HTTP_204_NO_CONTENT
       )
 
+  except:
+    return JsonResponse({'message': 'The post does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def post_comments(request: HttpRequest, id):
+  try:
+    comment_list = Comment.objects.filter(postId=id)
+    comment_serializer = CommentSerializer(comment_list, many=True)
+    return JsonResponse(comment_serializer.data, safe=False, status=status.HTTP_200_OK)
   except:
     return JsonResponse({'message': 'The post does not exist'}, status=status.HTTP_404_NOT_FOUND)
